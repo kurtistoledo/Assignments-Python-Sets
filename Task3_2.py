b4329e41dee3e3e08fc56e2b8dcb79606abdd945
Kurtis Toledo <kurtistoledo@gmail.com>
2024-05-25 08:53:45 -0500

    Task3_2.py

---
+++ Task3_2.py
@@ -0,0 +1,9 @@
+# Lesson 3: Python Sets
+# 2. Set Operations in Data Analysis
+
+customer_ids = ["C001", "C002", "C003", "C002", "C001", "C004"]
+
+# Convert the list to a set to remove duplicates
+unique_customer_ids = set(customer_ids)
+
+print(f"Unique customer IDs: {unique_customer_ids}")
