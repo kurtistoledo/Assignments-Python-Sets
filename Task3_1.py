8e0f7d3a850f870a41da838eca4588264b61ff12
Kurtis Toledo <kurtistoledo@gmail.com>
2024-05-25 08:53:21 -0500

    Task3_1.py

---
+++ Task3_1.py
@@ -0,0 +1,12 @@
+
+
+our_routes = {"LAX", "JFK", "CDG", "DXB"}
+competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
+
+common_destinations = our_routes.intersection(competitor_routes)
+unique_to_our_airline = our_routes - competitor_routes
+neither_shared_destinations = our_routes.symmetric_difference(competitor_routes)
+
+print(f"Common destinations: {common_destinations}")
+print(f"Destinations unique to our airline: {unique_to_our_airline}")
+print(f"Destinations neither airline shares: {neither_shared_destinations}")
